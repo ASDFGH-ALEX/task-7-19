f4ac6b849993bf23eee5cc161ce01577d5390d42
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-10-19 15:52:38 +0530

    Add files via upload

---
+++ task 2 of 19.py
@@ -0,0 +1,65 @@
+'''
+Author:Alex SAbu
+Date:19-10-2024
+Description:a Python program that performs the following tasks:
+
+    Create two separate strings:
+        The first string should contain your first name.
+        The second string should contain your last name.
+
+    Concatenate the two strings with a space in between and store the result in a new variable.
+
+    Print the concatenated string.
+
+    From the concatenated string:
+
+'''
+first_name=input("Enter your first name")
+second_name=input("Enter your second name")
+full_name=first_name+ " " +second_name
+print(full_name)
+str4=(full_name[5:])
+print(str4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
